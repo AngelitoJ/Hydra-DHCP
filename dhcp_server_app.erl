36a5d3ede8c3f83c3aa085ff4e9f5d8b955b9ad0
Angel J Alvarez Miguel <angel@uah.es>
2014-04-22 17:27:21 +0200

    Main OTP application module
    this sucker does nothing but to start and stop the app, yet in a future
    takeover logic will reside here too.

---
+++ dhcp_server_app.erl
@@ -0,0 +1,34 @@
+%% Hydra DHCP Server project
+%% (C) 2014 Angel J. Alvarez Miguel
+
+
+
+-module(dhcp_server_app).
+-author("angeljalvarezmiguel@gmail.com").
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, StartArgs) ->
+
+   {ok, CmdlineOpts} = application:get_env(cmdline_options),    %% recover Opts from the enviroment
+   AppOptions = [{start_args,StartArgs}|CmdlineOpts],           %% Combine start aargs with cmdline options
+
+   case dhcp_server_sup:start_link(AppOptions) of
+   {ok, Pid} ->
+       io:format("Application started...\n\n"),
+      {ok, Pid};
+   {error,Other} ->
+      io:format("Application crashed! Error: ~p\n",[Other]),
+      {error, Other}
+   end.
+
+stop(_State) ->
+    io:format("Application stopped...\n\n"),
+    ok.
+
