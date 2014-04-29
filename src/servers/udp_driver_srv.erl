e8c7d90f73a624129c81c73cf14588d377d069b6
Angel J Alvarez Miguel <angel@uah.es>
2014-04-29 09:14:37 +0200

    Prepare udp_driver for unit testing with unit
    provided full exports if we are running tests or just public functions
    for production code.

--- src/servers/udp_driver_srv.erl
+++ src/servers/udp_driver_srv.erl
@@ -11,6 +11,13 @@
                         ,pool    = empty      %%  {Used,Rest} | empty, pool of Middlemen available in a round-robin fashion 
                       }).
 
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+-compile(export_all).
+
+-else.
+
 %% ------------------------------------------------------------------
 %% API Function Exports
 %% ------------------------------------------------------------------
@@ -23,6 +30,7 @@
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
+-endif.
 
 %% ------------------------------------------------------------------
 %% API Function Definitions
