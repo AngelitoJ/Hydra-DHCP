79c824bff1e92afef643f82e44e779e74b69aa47
Angel J Alvarez Miguel <angel@uah.es>
2014-05-11 11:38:09 +0200

    Spawn as many pool servers as pool files to manage
    Logic to span a dynamic number of workers upon the number of pools we
    have to manage and serve

--- src/supervisors/addr_pool_sup.erl
+++ src/supervisors/addr_pool_sup.erl
@@ -12,7 +12,7 @@
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
--define(CHILD(I, Type, Args), {I, {I, start_link, [Args]}, permanent, 5000, Type, [I]}).
+-define(CHILD(I, Type, Mod, Args), {I, {Mod, start_link, [Args]}, permanent, 5000, Type, [Mod]}).
 
 %% ===================================================================
 %% API functions
@@ -30,12 +30,12 @@ option_specs() ->
 %% Check valid pool dir (and check pool files inside)
 -spec check_pool_dir([any()]) -> [any()].
 check_pool_dir(Opts) ->
-    Dir        = proplists:get_value(datadir,Opts,"./data"),
+    Dir        = proplists:get_value(pool_dir,Opts,"./data"),
     Acc = filelib:fold_files(Dir
-                                ,"pool*.dat"                                       %% Look for pool files
-                                ,false                                             %% Not recursively
+                                ,".*"                                               %% Look for pool files
+                                ,false                                              %% Not recursively
                                 ,fun(File,{Bool,List}) ->
-                                                    case filelib:is_file(File) of  %% check pool file is valid (its fake check )
+                                                    case filelib:is_file(File) of   %% check pool file is valid (its fake check )
                                                         true -> {Bool,[File|List]};
                                                         false -> {false,File}
                                                     end
@@ -53,7 +53,20 @@ check_pool_dir(Opts) ->
 %% ===================================================================
 
 init(Opts) ->
-    io:format("~p: Init with Args: ~w\n", [?MODULE,Opts]),
+    case proplists:get_value(pool_files,Opts,undefined) of
+    	undefined -> 
+    				{error,no_pools};
+    	Pools     ->
+    				ChildrenSpec = [ ?CHILD(X, worker, addr_pool_srv, [{who_you_are,srv_id(X)}]) 
+    								|| X <- lists:seq(1,length(Pools)) ],
+    				io:format("[~p] Initiating, got ~p address pools to setup\n", [?MODULE,length(Pools)]),
+    				{ok, { {one_for_one, 5, 10}, ChildrenSpec} }
+    end.
 
-    {ok, { {one_for_one, 5, 10}, []} }.
+%% Make a new atom to name a middlemen server
+srv_id(Id) ->
+	Server_name = "addr_pool_srv_" ++ integer_to_list(Id),
+	list_to_atom(Server_name).
+
+
 
