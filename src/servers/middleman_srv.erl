abd033b56410080350a1b86b4303c8eae37e39e9
Angel J Alvarez Miguel <angel@uah.es>
2014-05-11 20:18:37 +0200

    Move DHCP option related function to a new module
    Refactor non process related funs into a module.

--- src/servers/middleman_srv.erl
+++ src/servers/middleman_srv.erl
@@ -22,6 +22,7 @@
 
 %% DHCP related functions
 -include("dhcp_messages.hrl").
+-include("address_tools.hrl").
 
 -record(middleman_state, {
                              id        = undefined            %% This middleman server ID
@@ -179,9 +180,9 @@ get_client_id(#dhcp_packet{ chaddr = Id }) -> Id.
 
 %% map a DHCP binary packet into a record for easy processing.
 packet_to_record(?DHCP_PACKET) ->
-  DecodedOptions                      = dhcp_decode_options(Options),
-  {message_type, MessageType}         = dhcp_option_search(message_type, DecodedOptions),
-  {requested_ip_address, RequestedIp} = dhcp_option_search(requested_ip_address,DecodedOptions),
+  DecodedOptions                      = dhcp_options:decode_options(Options),
+  {message_type, MessageType}         = dhcp_options:option_search(message_type, DecodedOptions),
+  {requested_ip_address, RequestedIp} = dhcp_options:option_search(requested_ip_address,DecodedOptions),
 
 
   Record = #dhcp_packet{
@@ -215,7 +216,7 @@ record_to_packet(#dhcp_packet{ op = Op, htype = HType, hlen = HLen, hops = Hops,
             , chaddr = CHAddr, sname = SName, file = File, options = DecodedOptions, msg_type = MessageType
             , rqaddr = RequestedIp, client_unicast = ClientScope } = Packet) ->
 
-  EncodedOptions = dhcp_encode_options(DecodedOptions),
+  EncodedOptions = dhcp_options:encode_options(DecodedOptions),
   EncodedPacket = <<Op:8, HType:8, HLen:8, Hops:8, Xid:32, Secs:16, Flags:16/big
             ,(tuple_to_ipv4(CIAddr))/binary, (tuple_to_ipv4(YIAddr))/binary
             ,(tuple_to_ipv4(SIAddr))/binary, (tuple_to_ipv4(GIAddr))/binary
@@ -227,210 +228,4 @@ record_to_packet(#dhcp_packet{ op = Op, htype = HType, hlen = HLen, hops = Hops,
 record_to_packet(_) ->
   {error, unknown_record}.
 
-
-%% IPv4 conversion functions <<A:8,B:8,C:8,D:8>> <--> {A,B,C,D}
-ipv4_to_tuple(<<A:8, B:8, C:8, D:8>>) -> {A, B, C, D}.
-tuple_to_ipv4({A,B,C,D})              ->  <<A:8, B:8, C:8, D:8>>.
-
-%% MAC 802.3 conversion functions <<A:8,B:8,C:8,D:8,E:8,F:8, _/binary>> <--> {A,B,C,D,E,F}
-mac_to_tuple(<<A:8, B:8, C:8, D:8, E:8, F:8, _/binary>>) -> {A, B, C, D, E, F}.
-tuple_to_mac({A, B, C, D, E, F}) ->  <<A:8, B:8, C:8, D:8, E:8, F:8, 0:80>>.
-
-
-%% map DHCP options binary to a property list 
-dhcp_decode_options(Binary) -> dhcp_decode_options(Binary, []).
-dhcp_decode_options(<<0:8, Rest/binary>>, []) -> dhcp_decode_options(Rest, []);
-dhcp_decode_options(<<255:8, _/binary>>, Opts) -> Opts;                            % Final Marker. We are done
-dhcp_decode_options(<<Op:8, Len:8, Data:Len/binary, Rest/binary>>, Opts) ->        % Default
-  Name  = dhcp_decode_option_name(Op),
-  Value = dhcp_decode_option_value(Name, Data),
-  dhcp_decode_options(Rest,[{Name, Value }|Opts]).
-
-%% map DHCP options property list into a binary
-dhcp_encode_options([{Name, Val} | Rest]) ->
-  Op      = dhcp_encode_option_name(Name),
-  Data    = dhcp_encode_option_value(Name, Val),
-  Len     = size(Data),
-  <<Op:8, Len:8, Data/binary, (dhcp_encode_options(Rest))/binary>>;
-dhcp_encode_options([]) -> <<255:8>>.                                               % We are done!! (i guess..)
-
-
-%% search for DHCP options and provide sane defaults.
-dhcp_option_search(requested_ip_address,Opts) -> 
-  case lists:keysearch(requested_ip_address, 1, Opts) of
-    {value, Value} -> Value;
-        _              -> {requested_ip_address, {0, 0, 0, 0}}
-    end;
-dhcp_option_search(Key,Opts) ->
-  case lists:keysearch(Key, 1, Opts) of
-    {value, Value} -> Value;
-        Other          -> Other
-    end.
-
-
-
-%% decode a option number into a symbolic name, not all option are implemented..
-dhcp_decode_option_name(1) ->  subnet_mask;
-dhcp_decode_option_name(2) ->  time_offset;
-dhcp_decode_option_name(3) ->  router;
-  %     4 : "Time Server Option",
-  %     5 : "Name Server Option",
-dhcp_decode_option_name(6) ->  dns_server;
-  %     7 : "Log Server Option",
-  %     8 : "Cookie Server Option",
-  %     9 : "LPR Server Option",
-  %    10 : "Impress Server Option",
-  %    11 : "Resource Location Server Option",
-dhcp_decode_option_name(12) -> host_name;
-  %    13 : "Boot File Size Option",
-  %    14 : "Merit Dump File",
-dhcp_decode_option_name(15) -> domain_name;
-  %    16 : "Swap Server",
-  %    17 : "Root Path",
-  %    18 : "Extensions Path",
-  %    19 : "IP Forwarding Enable/Disable Option",
-  %    20 : "Non-Local Source Routing Enable/Disable Option",
-  %    21 : "Policy Filter Option",
-  %    22 : "Maximum Datagram Reassembly Size",
-  %    23 : "Default IP Time-to-live",
-  %    24 : "Path MTU Aging Timeout Option",
-  %    25 : "Path MTU Plateau Table Option",
-  %    26 : "Interface MTU Option",
-  %    27 : "All Subnets are Local Option",
-dhcp_decode_option_name(28) -> broadcast_address;
-  %    29 : "Perform Mask Discovery Option",
-  %    30 : "Mask Supplier Option",
-  %    31 : "Perform Router Discovery Option",
-  %    32 : "Router Solicitation Address Option",
-  %    33 : "Static Route Option",
-  %    34 : "Trailer Encapsulation Option",
-  %    35 : "ARP Cache Timeout Option",
-  %    36 : "Ethernet Encapsulation Option",
-  %    37 : "TCP Default TTL Option",
-  %    38 : "TCP Keepalive Interval Option",
-  %    39 : "TCP Keepalive Garbage Option",
-  %    40 : "Network Information Service Domain Option",
-  %    41 : "Network Information Servers Option",
-  %    42 : "Network Time Protocol Servers Option",
-  %    43 : "Vendor Specific Information",
-  %    44 : "NetBIOS over TCP/IP Name Server Option",
-  %    45 : "NetBIOS over TCP/IP Datagram Distribution Server Option",
-  %    46 : "NetBIOS over TCP/IP Node Type Option",
-  %    47 : "NetBIOS over TCP/IP Scope Option",
-  %    48 : "X Window System Font Server Option",
-  %    49 : "X Window System Display Manager Option",
-dhcp_decode_option_name(50) -> requested_ip_address;
-dhcp_decode_option_name(51) -> lease_time;
-dhcp_decode_option_name(53) -> message_type;
-dhcp_decode_option_name(54) -> server_id;
-dhcp_decode_option_name(55) -> parameter_request;
-  %    56 : "Message",
-dhcp_decode_option_name(57) -> max_message_size;
-dhcp_decode_option_name(58) -> renewal_time;
-dhcp_decode_option_name(59) -> rebinding_time;
-  %    60 : "Vendor class identifier",
-dhcp_decode_option_name(61) -> client_id;
-  %    64 : "Network Information Service+ Domain Option",
-  %    65 : "Network Information Service+ Servers Option",
-  %    66 : "TFTP server name",
-  %    67 : "Bootfile name",
-  %    68 : "Mobile IP Home Agent option",
-  %    69 : "Simple Mail Transport Protocol (SMTP) Server Option",
-  %    70 : "Post Office Protocol (POP3) Server Option",
-  %    71 : "Network News Transport Protocol (NNTP) Server Option",
-  %    72 : "Default World Wide Web (WWW) Server Option",
-  %    73 : "Default Finger Server Option",
-  %    74 : "Default Internet Relay Chat (IRC) Server Option",
-  %    75 : "StreetTalk Server Option",
-  %    76 : "StreetTalk Directory Assistance (STDA) Server Option",
-dhcp_decode_option_name(Op) ->
-  io:format("Error decoding DHCP Option: ~p~n", [Op]),
-  error.          %% I know, this should be an error..
-
-%% decode a option number into a symbolic name
-dhcp_encode_option_name(subnet_mask)          -> 1;
-dhcp_encode_option_name(time_offset)          -> 2;
-dhcp_encode_option_name(router)               -> 3;
-dhcp_encode_option_name(dns_server)           -> 6;
-dhcp_encode_option_name(host_name)            -> 12;
-dhcp_encode_option_name(domain_name)          -> 15;
-dhcp_encode_option_name(broadcast_address)    -> 28;
-dhcp_encode_option_name(requested_ip_address) -> 50;
-dhcp_encode_option_name(lease_time)           -> 51;
-dhcp_encode_option_name(message_type)         -> 53;
-dhcp_encode_option_name(server_id)            -> 54;
-dhcp_encode_option_name(parameter_request)    -> 55;
-dhcp_encode_option_name(max_message_size)     -> 57;
-dhcp_encode_option_name(renewal_time)         -> 58;
-dhcp_encode_option_name(rebinding_time)       -> 59;
-dhcp_encode_option_name(client_id)            -> 61;
-dhcp_encode_option_name(Op) ->
-  io:format("Error encoding DHCP Option: ~p~n", [Op]),
-  115.              %% Seems to be unasigned..
-
-
-%% Decode every known DHCP option
-dhcp_decode_option_value(subnet_mask, Value)          -> decode_value(ip_address, Value);
-dhcp_decode_option_value(router, Value)               -> decode_value(ip_address, Value);
-dhcp_decode_option_value(dns_server, Value)           -> decode_value(ip_address, Value);
-dhcp_decode_option_value(broadcast_address, Value)    -> decode_value(ip_address, Value);
-dhcp_decode_option_value(requested_ip_address, Value) -> decode_value(ip_address, Value);
-dhcp_decode_option_value(server_id, Value)            -> decode_value(ip_address, Value);
-dhcp_decode_option_value(time_offset, Value)          -> decode_value(quad_int, Value);
-dhcp_decode_option_value(lease_time, Value)           -> decode_value(quad_int, Value);
-dhcp_decode_option_value(renewal_time, Value)         -> decode_value(quad_int, Value);
-dhcp_decode_option_value(rebinding_time, Value)       -> decode_value(quad_int, Value);
-dhcp_decode_option_value(max_message_size, Value)     -> decode_value(word_int, Value);
-dhcp_decode_option_value(host_name, Value)            -> decode_value(string, Value);
-dhcp_decode_option_value(domain_name, Value)          -> decode_value(string, Value);
-dhcp_decode_option_value(message_type, Value)         -> decode_value(message_type, Value);
-dhcp_decode_option_value(_, Value)                    -> decode_value(binary, Value).
-
-%% Decode all diferent kind of values in a DHCP option field 
-decode_value(binary, Value)              -> Value;
-decode_value(ip_address, Value)          -> ipv4_to_tuple(Value);
-decode_value(string, Value)              -> binary_to_list(Value);
-decode_value(quad_int, <<DWord:32/big>>) -> DWord;
-decode_value(word_int, <<Word:16/big>>)  -> Word;
-decode_value(message_type, <<1:8>>)      -> discover;
-decode_value(message_type, <<2:8>>)      -> offer;
-decode_value(message_type, <<3:8>>)      -> request;
-decode_value(message_type, <<4:8>>)      -> decline;
-decode_value(message_type, <<5:8>>)      -> ack;
-decode_value(message_type, <<6:8>>)      -> nak;
-decode_value(message_type, <<7:8>>)      -> release;
-decode_value(message_type, <<8:8>>)      -> inform.
-
-%% Encode every known DHCP option
-dhcp_encode_option_value(subnet_mask, Value)          -> encode_value(ip_address, Value);
-dhcp_encode_option_value(router, Value)               -> encode_value(ip_address, Value);
-dhcp_encode_option_value(dns_server, Value)           -> encode_value(ip_address, Value);
-dhcp_encode_option_value(broadcast_address, Value)    -> encode_value(ip_address, Value);
-dhcp_encode_option_value(requested_ip_address, Value) -> encode_value(ip_address, Value);
-dhcp_encode_option_value(server_id, Value)            -> encode_value(ip_address, Value);
-dhcp_encode_option_value(time_offset, Value)          -> encode_value(quad_int, Value);
-dhcp_encode_option_value(lease_time, Value)           -> encode_value(quad_int, Value);
-dhcp_encode_option_value(renewal_time, Value)         -> encode_value(quad_int, Value);
-dhcp_encode_option_value(rebinding_time, Value)       -> encode_value(quad_int, Value);
-dhcp_encode_option_value(max_message_size, Value)     -> encode_value(word_int, Value);
-dhcp_encode_option_value(host_name, Value)            -> encode_value(string, Value);
-dhcp_encode_option_value(domain_name, Value)          -> encode_value(string, Value);
-dhcp_encode_option_value(message_type, Value)         -> encode_value(message_type, Value);
-dhcp_encode_option_value(_, Value)                    -> encode_value(binary, Value).
-
-%% Encode all diferent kind of values in a DHCP option field 
-encode_value(binary, Value)                      -> Value;
-encode_value(ip_address, Value)                  -> tuple_to_ipv4(Value);
-encode_value(string, Value)                      -> list_to_binary(Value);
-encode_value(quad_int, Value)                    -> <<Value:32/big>>;
-encode_value(word_int, Value)                    -> <<Value:16/big>>;
-encode_value(message_type, discover)             -> <<1:8>>;
-encode_value(message_type, offer)                -> <<2:8>>;
-encode_value(message_type, request)              -> <<3:8>>;
-encode_value(message_type, decline)              -> <<4:8>>;
-encode_value(message_type, ack)                  -> <<5:8>>;
-encode_value(message_type, nak)                  -> <<6:8>>;
-encode_value(message_type, release)              -> <<7:8>>;
-encode_value(message_type, inform)               -> <<8:8>>;
-encode_value(Name, Value)                        -> Value.
 
