eddbd0233b4e1bce775250a6cf8997a1663da686
AngelitoJ <angel@uah.es>
2014-05-31 21:22:51 +0200

    Simple table master
    A tables master acts as a heir of ets tables in case owners die.

--- src/servers/ets_master_srv.erl
+++ src/servers/ets_master_srv.erl
@@ -18,6 +18,10 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
+-record(st,{
+			tables = []      %% dict of {Name,Tid} this process knows about
+			}).
+
 %% ------------------------------------------------------------------
 %% API Function Definitions
 %% ------------------------------------------------------------------
@@ -30,9 +34,17 @@ start_link(Opts) ->
 %% ------------------------------------------------------------------
 
 init(Args) ->
-  io:format("~p: Init..\n", [?MODULE]),
+	io:format("~p: Init..\n", [?MODULE]),
 
-    {ok, Args}.
+    {ok, #st{ tables = dict:new() }}.
+
+%% Tell a process the Tid of a table (if we know that)
+handle_call({get, Name}, From, #st{ tables = Tables} = State) ->
+	{reply, dict:find(Name, Tables), State};
+
+%% Remove a Table in case the process wants to close it
+handle_call({remove, Name}, From, #st{ tables = Tables} = State) ->	
+    {reply, ok, State#st{ tables = dict:erase(Name,Tables) }};
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
@@ -40,7 +52,18 @@ handle_call(_Request, _From, State) ->
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-handle_info(_Info, State) ->
+
+%% Gain table ownership when some process screws up. Heirdata must contain the table Name
+handle_info({'ETS-TRANSFER', Tid, FromPid, {name = Name}}, #st{ tables = Tables } = State) ->
+	{noreply, State#st{ tables = dict:update(Name, fun(X) -> X end, Tid, Tables)}};  
+
+%% Failed attept to gain a table ownership because we dont know the table's name 
+handle_info({'ETS-TRANSFER', Tid, FromPid, Whatever}, State) ->
+	io:format("[TABLE MASTER]: Sorry!, process ~p died, but we can inherit its table\n", [FromPid]),
+	{noreply, State};  
+
+handle_info(Info, State) ->
+	io:format("[TABLE MASTER]: Sorry!, I received this: ~p and I dont like it...\n", [Info]),
     {noreply, State}.
 
 terminate(_Reason, _State) ->
