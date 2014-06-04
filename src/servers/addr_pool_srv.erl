007b4988d854fb0b9384ce6168d6f1e5a15f13a8
AngelitoJ <angel@uah.es>
2014-06-04 20:59:39 +0200

    Refactor state init
    Stream line state setup functions for the simple pool type, change
    names and manage pool (ETS) and leases (DETS) separately but in sync.

--- src/servers/addr_pool_srv.erl
+++ src/servers/addr_pool_srv.erl
@@ -20,14 +20,10 @@
 
 -include("address_tools.hrl").
 
--record(host, {                                 %% A host record describe a client Id holding a certain IP address
-                     id                         %% Client Id
-                    ,ip                         %% Address allocated to this client Id
-                }).
-
 -record(address, {                              %% An address record represents this basic allocation unit
                      ip                         %% IP address
                     ,status                     %% Status = AVAILABLE | OFFERED | ALLOCATED | EXPIRED..
+                    ,clientid                   %% ID of client holding the address if any.
                     ,options      = undefined   %% Options to this allocation
                 }).
 
@@ -120,70 +116,135 @@ code_change(_OldVsn, State, _Extra) ->
 load_pool(#st{ filepath = File } = State) ->
     {ok, [{pooldata,PoolData}|_]} = file:consult(File),
     case PoolData of
-        {simplepool, PoolOpts} -> lists:foldl(
-                                                 fun init_pool/2
-                                                ,State
-                                                ,PoolOpts
-                                            );
+        {simplepool, PoolOpts} -> load_simple_pool(State, PoolOpts);
         _ -> error("pool type not supported")
     end.
 
-%% Make a new addresses table pool (or recover it from the table heir) and a open its leases file (or create a new one)
-init_pool({name, Name}, #st{ id = Id } = State) ->
-    ETSName  = list_to_atom(Name),
-    DETSFile = Name ++ ".leases",
-    DETSName = list_to_atom(DETSFile),
+%% Load a simple pool from a property list 
+load_simple_pool(State, Opts) ->
+    {name, Name} = proplists:lookup(name, Opts),     
+    PoolName     = list_to_atom(Name),                     %% Atom for the ETS table name
+    LeasesFile   = Name ++ ".leases",                      %% String for the DETS table file
+    LeasesName   = list_to_atom(LeasesFile),               %% Atom for the DETS table name
+    PoolOpts     = [
+                     {poolname, PoolName}                   %% Name of the ETS pool
+                    ,{leasesname, LeasesName, LeasesFile}   %% declare a leases file attached tp this pool
+                    ] ++ proplists:delete(name, Opts),      %% delete old name property
 
-    io:format("[~p]: Creating ETS pool named ~p..\n", [Id,ETSName]),
+    %% Fold over the property list setting up state for every known option
+    NewState = lists:foldl(fun state_init/2, State ,PoolOpts),
+
+    %% Now we have to transfer active leases from the leases table to the pool table
+    leases_to_pool(NewState),
+    NewState.
+
+
+%% Create a new ETS table for the address pool (or recover it from the table heir) 
+state_init({poolname, Name}, #st{ id = Id } = State) ->
+
+    io:format("[~p]: Creating ETS pool named ~p..\n", [Id,Name]),
     %% Ask Table heir if table already exists, create a new one if needed and set the heir
-    Tidpool = case gen_server:call(ets_master_srv, {get, ETSName}) of     
-          {ok, Value}       -> Value;
-          {not_found, Pid}  -> ets:new(ETSName, [                            
+    Tidpool = case gen_server:call(ets_master_srv, {get, Name}) of     
+        {ok, Value}       -> Value;
+        {not_found, Pid}  -> ets:new(Name, [                            
                                                  public
                                                 ,{keypos, #address.ip}
-                                                ,{heir, Pid, {name, ETSName}}
+                                                ,{heir, Pid, {name, Name}}
                                             ])
-          end,
-    %% Open de lease file or die..
-    {ok, Tidleases} = dets:open_file(DETSName, [
-                                                   {keypos, #lease.clientid}
-                                                 ,{file, DETSFile}
+        end,
+    State#st{ pool = Tidpool };
+
+%% Open or recreate a DETS file to persist lease information
+state_init({leasesname, Name, File}, #st{ id = Id } = State) -> 
+
+    io:format("[~p]: Creating DETS leases table named ~p..\n", [Id, Name]),
+    %% Open lease file or die..
+    {ok, Tidleases} = dets:open_file(Name, [
+                                                  {keypos, #lease.clientid}
+                                                 ,{file, File}
                                             ]),
 
-    State#st{ pool = Tidpool, leases =  Tidleases };
+    State#st{ leases =  Tidleases };
+
 
 %% Populate the ETS table with free addresses         
-init_pool({range, Start, End}, #st{ id = Id, pool = Tid } = State) ->
+state_init({range, Start, End}, #st{ id = Id, pool = Tid } = State) ->
     io:format("[~p]: Populating pool from ~p to ~p..\n", [Id,Start,End]),
-    populate(Tid, free, Start, End),
+    pool_populate(
+                     Tid
+                    ,fun(IP) -> %%io:format("[~p]: Population pool with address ~p\n",[Id, IP]),
+                                #address{ ip = IP, status = free , clientid = undefined} end
+                    ,Start
+                    ,End
+                    ),
     State;
 
 %% record the pool client options
-init_pool({options, Options}, #st{ id = Id } = State) ->
+state_init({options, Options}, #st{ id = Id } = State) ->
     lists:foreach(
                     fun(I) -> io:format("[~p]: option: ~p\n",[Id, I]) end,
                     Options
                 ),
     State#st{ options = Options };
 
-init_pool(Tuple,#st{ id = Id } = State) ->
+state_init(Tuple,#st{ id = Id } = State) ->
     io:format("[~p]: Unknow Pool setting ~p:\n", [Id,Tuple]),
     State.
 
-%% Populate the given table with address records on a given status, use explicit tail recursion to avoid building a large list
-populate(Tid, Status, Addr, Addr) ->
-%%    io:format("Addres: ~p Status: ~p\n",[Addr,Status]),
-    ets:insert(Tid, #address{
-                                 ip     = Addr
-                                ,status = Status
-                            });
-populate(Tid, Status, Addr, End) ->
-%%    io:format("Addres: ~p Status: ~p\n",[Addr,Status]),
-    ets:insert(Tid, #address{
-                                 ip     = Addr
-                                ,status = Status
-                            }),
-    populate(Tid, Status, ipv4_succ(Addr), End).
+%% Lookup a IP address and tag it accordingly free | offered | not_found
+pool_lookup(State, Ip) ->
+    case ets:lookup(State#st.pool, Ip) of
+        [Address] when Address#address.status == free    -> {free, Address};
+        [Address] when Address#address.status == offered -> {offered, Address};
+        _                                                -> not_found
+    end.
+%% Insert or replace an addres object in the pool
+pool_insert(State, Address) when is_record(Address, address) ->
+    ets:insert(State#st.pool, Address).
+
+%% Populate the given table with address records on a given status (provided by a fun)
+%% use explicit tail recursion to avoid building a large list
+pool_populate(Tid, Fun, Addr, Addr) when is_function(Fun) ->
+    ets:insert(Tid, Fun(Addr));
+
+pool_populate(Tid, Fun, Addr, End) ->
+    ets:insert(Tid, Fun(Addr)),
+    pool_populate(Tid, Fun, ipv4_succ(Addr), End).
+
+%% Lookup a client lease and tag it accordingly ok | expired | not_found
+leases_lookup(State, Id, Now) ->
+    case dets:lookup(State#st.leases, Id) of
+        [Lease] when Lease#lease.expires > Now -> {ok, Lease};
+        [Lease]                                -> {expired, Lease};         
+        []                                     -> not_found
+    end.
+
+%% Insert a new lease
+leases_insert(State, Lease) when is_record(Lease, lease) ->
+    ok = dets:insert(State#st.leases, Lease).
+
+%% delete a client lease
+leases_remove(State, Id) -> 
+    ok = dets:delete(State#st.leases, Id).
+
+%% Transfer active leases to pool folding over the dets table to avoid collecting large results
+leases_to_pool(#st{ id = Id, options = Options, leases = Tidleases } = State) ->
+    Now = calendar:datetime_to_gregorian_seconds({date(), time()}),
+    dets:foldl(
+                fun(Lease, Acc) -> case Lease#lease.expires > Now of
+                                   %% Populate the pool with the address extraacted from the list
+                                   true  -> io:format("[~p]: Transferring client ~p lease to pool.\n",[Id,Lease#lease.clientid]),
+                                            pool_insert(State, #address{ 
+                                                             ip       = Lease#lease.ip
+                                                            ,options  = Options
+                                                            ,clientid = Lease#lease.clientid 
+                                                            }),
+                                    Acc;
+                                    false -> Acc
+                                    end
+                end
+                ,[]
+                ,Tidleases).
 
 
 %% Try to select the first free IP address available
@@ -198,12 +259,12 @@ select_address(#st{ options = Options } = State, ClientId, RequestedIP) ->
     Now = calendar:datetime_to_gregorian_seconds({date(), time()}),
 
     case leases_lookup(State, ClientId, Now) of
-        %% There is a active lease, we use it 
+        %% There is a active lease, we use it ignoring the client requested IP
         {ok, Lease}      -> {ok, #address{ip = Lease#lease.ip, options = Options}};
 
         %% There is a expired lease, we have to check the pool                                                                
         {expired, Lease} -> case pool_lookup(State, Lease#lease.ip) of
-                            %% The address is free we can use it anyway
+                            %% The address is free we can reuse it anyway
                             {free, Address}    -> {ok, Address#address{options = Options}};
                             %% The address is not free , we remove the lease and try again
                             _                  -> leases_remove(State, ClientId),
@@ -251,36 +312,6 @@ allocate_address(State, Address, ClientId) ->
         pool_insert(State, Address#address{status = allocated}),
         {ok, Address#address.ip, Address#address.options }
     end.
-
-
-%% Lookup a client lease and tag it accordingly ok | expired | not_found
-leases_lookup(State, Id, Now) ->
-    case dets:lookup(State#st.leases, Id) of
-        [Lease] when Lease#lease.expires > Now -> {ok, Lease};
-        [Lease]                                -> {expired, Lease};         
-        []                                     -> not_found
-    end.
-
-%% Insert a new lease
-leases_insert(State, Lease) when is_record(Lease, lease) ->
-    ok = dets:insert(State#st.leases, Lease).
-
-%% delete a client lease
-leases_remove(State, Id) -> 
-    ok = dets:delete(State#st.leases, Id).
-
-%% Lookup a IP address and tag it accordingly free | offered | not_found
-pool_lookup(State, Ip) ->
-    case ets:lookup(State#st.pool, Ip) of
-        [Address] when Address#address.status == free    -> {free, Address};
-        [Address] when Address#address.status == offered -> {offered, Address};
-        _                                                -> not_found
-    end.
-%% Insert or replace an addres object in the pool
-pool_insert(State, Address) when is_record(Address, address) ->
-    ets:insert(State#st.pool, Address).
-
-
 
 
 
