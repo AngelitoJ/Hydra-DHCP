eff061a6a2211cc60103fb4025b8db065aaee8d7
AngelitoJ <angel@uah.es>
2014-06-07 10:09:42 +0200

    put some comments to the functions

--- src/utils/cache.erl
+++ src/utils/cache.erl
@@ -13,23 +13,24 @@
                 }).
 
 
-
+%% Make a fresh pid cache 
 new() -> #pidcache{ 
                      pids = dict:new()
                     ,ids = dict:new()
                     }.
-
+%% Insert a id -> pid entry
 insert(#pidcache{ pids = Pids, ids = Ids }, Id, Pid) ->
     #pidcache{ 
                  pids = dict:store(Id, Pid, Pids)
                 ,ids  = dict:store(Pid, Id, Ids) }.
 
-
+%% Lookup an ID resolving it to its pid (if found)
 lookup_by_id(#pidcache{ pids = Pids }, Id) -> dict:find(Id, Pids).   %% id -> pid
 
+%% Lookup a Pid resolving it to its ID (if found)
 lookup_by_pid(#pidcache{ ids = Ids }, Pid) -> dict:find(Pid, Ids).   %% pid -> id
 
-
+%% remove an entry by its PID (do nothing if not found)
 remove_by_pid(#pidcache{ pids= Pids, ids = Ids } = Cache, Pid) ->
     case dict:find(Pid, Ids) of
         {ok, Id} -> #pidcache{ 
